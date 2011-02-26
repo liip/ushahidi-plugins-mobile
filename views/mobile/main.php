14aa5381ee439e596a5c49dd39f60cabfe4ff5ea
lejoe <copernic67@lejoe.com>
2011-02-26 16:14:01 +0100

    Made the form not expandable

--- views/mobile/main.php
+++ views/mobile/main.php
@@ -2,7 +2,7 @@
 	<h2 class="submit"><a href="<?php echo url::site()."mobile/reports/submit/" ?>">Submit A Report</a></h2>
 </div>
 <div class="block">
-	<h2 class="expand">Find a Report</h2>
+	<h2 class="other">Find a Report</h2>
 	<div class="collapse shown">
 		<?php
 			echo '<form action="'.url::site().'mobile/reports/search" method="get">';
