58cf23ef7d2ae4c2553a1a861201ebad65e1bb17
lejoe <copernic67@lejoe.com>
2011-02-26 19:49:52 +0100

    Adding a filter bar

--- views/mobile/reports.php
+++ views/mobile/reports.php
@@ -12,6 +12,43 @@
 			$category_id = "";
 		}
 		?>
+		<div class="other filter">
+			<form action=<?php echo url::site() . '/mobile/reports/search' ?> method="get" accept-charset="utf-8">
+				<label for="town">Your town</label>
+				<input type="text" name="town" value="<?php echo isset($_GET['town']) ? $_GET['town'] : ''; ?>">
+				<label for="distance">Distance</label>
+				<select name="distance" id="distance">
+					<?php
+					$distance_options = array('0.5', '1', '2', '3', '7', '10', '20');
+					$selected_distance = isset($_GET['distance']) ? $_GET['distance'] : '0.5';
+					foreach($distance_options as $option){
+						echo '<option value="' . $option . '"';
+						echo $option == $selected_distance ? 'selected' : '';
+						echo '>' . $option . ' km</option>';
+					}
+					?>
+				</select>
+				<label for="category">Category</label>
+				<select name="category" id="category">
+					<option value="0">all</option>
+					<option value="la">la</option>
+					<option value="li">li</option>
+				</select>
+				<label for="order">Order By</label>
+				<select name="order" id="order">
+					<?php
+					$order_options = array('distance', 'date', 'verified');
+					$selected_distance = isset($_GET['order']) ? $_GET['order'] : 'distance';
+					foreach($order_options as $option){
+						echo '<option value="' . $option . '"';
+						echo $option == $selected_distance ? 'selected' : '';
+						echo '>' . $option . '</option>';
+					}
+					?>
+				</select>
+				<input type="submit" value="Search &rarr;">
+			</form>
+		</div>
 		<div class="list">
 			<ul>
 				<?php
