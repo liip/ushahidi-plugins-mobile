3e648e04cc5a7b3be2be993d175642d031c1c71f
Ivan Jovanovic <ivan@local.ch>
2011-02-26 14:40:55 +0100

    Removed not-needed DB queries from the current simplified home page

--- controllers/mobile.php
+++ controllers/mobile.php
@@ -60,65 +60,6 @@ class Mobile_Controller extends Template_Controller {
 	public function index()
 	{
 		$this->template->content  = new View('mobile/main');
-		
-		// Get 10 Most Recent Reports
-		$this->template->content->incidents = ORM::factory('incident')
-            ->where('incident_active', '1')
-			->limit('10')
-            ->orderby('incident_date', 'desc')
-			->with('location')
-            ->find_all();
-		
-		// Get all active top level categories
-        $parent_categories = array();
-        foreach (ORM::factory('category')
-				->where('category_visible', '1')
-				->where('parent_id', '0')
-				->orderby('category_title')
-				->find_all() as $category)
-        {
-            // Get The Children
-			$children = array();
-			foreach ($category->children as $child)
-			{
-				$children[$child->id] = array(
-					$child->category_title,
-					$child->category_color,
-					$child->category_image,
-					$this->_category_count($child->id)
-				);
-			}
-
-			// Put it all together
-            $parent_categories[$category->id] = array(
-				$category->category_title,
-				$category->category_color,
-				$category->category_image,
-				$this->_category_count($category->id),
-				$children
-			);
-			
-			if ($category->category_trusted)
-			{ // Get Trusted Category Count
-				$trusted = ORM::factory("incident")
-					->join("incident_category","incident.id","incident_category.incident_id")
-					->where("category_id",$category->id);
-				if ( ! $trusted->count_all())
-				{
-					unset($parent_categories[$category->id]);
-				}
-			}
-        }
-        $this->template->content->categories = $parent_categories;
-
-		// Get RSS News Feeds
-		$this->template->content->feeds = ORM::factory('feed_item')
-			->limit('10')
-            ->orderby('item_date', 'desc')
-            ->find_all();
-
-		// Get pages
-		$this->template->content->pages = ORM::factory('page')->where('page_active', '1')->find_all();
 	}
 	
 	private function _category_count($category_id = false)
